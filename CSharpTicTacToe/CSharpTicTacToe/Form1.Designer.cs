9f837bf21e04f9b5256770f290cfd1b5bd5aca8e
Araf Al-Jami <clown1331@yahoo.com>
2016-12-20 23:40:11 +0600

    CSharpTicTacToe

---
+++ CSharpTicTacToe/CSharpTicTacToe/Form1.Designer.cs
@@ -0,0 +1,196 @@
+﻿namespace CSharpTicTacToe
+{
+    partial class MainWin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1_1 = new System.Windows.Forms.Button();
+            this.button1_2 = new System.Windows.Forms.Button();
+            this.button1_3 = new System.Windows.Forms.Button();
+            this.button2_3 = new System.Windows.Forms.Button();
+            this.button2_2 = new System.Windows.Forms.Button();
+            this.button2_1 = new System.Windows.Forms.Button();
+            this.button3_3 = new System.Windows.Forms.Button();
+            this.button3_2 = new System.Windows.Forms.Button();
+            this.button3_1 = new System.Windows.Forms.Button();
+            this.button_newgame = new System.Windows.Forms.Button();
+            this.labelTurn = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1_1
+            // 
+            this.button1_1.Font = new System.Drawing.Font("Consolas", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1_1.Location = new System.Drawing.Point(26, 55);
+            this.button1_1.Name = "button1_1";
+            this.button1_1.Size = new System.Drawing.Size(40, 40);
+            this.button1_1.TabIndex = 0;
+            this.button1_1.UseVisualStyleBackColor = true;
+            this.button1_1.Click += new System.EventHandler(this.buttonClick);
+            // 
+            // button1_2
+            // 
+            this.button1_2.Font = new System.Drawing.Font("Consolas", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1_2.Location = new System.Drawing.Point(72, 55);
+            this.button1_2.Name = "button1_2";
+            this.button1_2.Size = new System.Drawing.Size(40, 40);
+            this.button1_2.TabIndex = 1;
+            this.button1_2.UseVisualStyleBackColor = true;
+            this.button1_2.Click += new System.EventHandler(this.buttonClick);
+            // 
+            // button1_3
+            // 
+            this.button1_3.Font = new System.Drawing.Font("Consolas", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1_3.Location = new System.Drawing.Point(118, 55);
+            this.button1_3.Name = "button1_3";
+            this.button1_3.Size = new System.Drawing.Size(40, 40);
+            this.button1_3.TabIndex = 2;
+            this.button1_3.UseVisualStyleBackColor = true;
+            this.button1_3.Click += new System.EventHandler(this.buttonClick);
+            // 
+            // button2_3
+            // 
+            this.button2_3.Font = new System.Drawing.Font("Consolas", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2_3.Location = new System.Drawing.Point(118, 101);
+            this.button2_3.Name = "button2_3";
+            this.button2_3.Size = new System.Drawing.Size(40, 40);
+            this.button2_3.TabIndex = 5;
+            this.button2_3.UseVisualStyleBackColor = true;
+            this.button2_3.Click += new System.EventHandler(this.buttonClick);
+            // 
+            // button2_2
+            // 
+            this.button2_2.Font = new System.Drawing.Font("Consolas", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2_2.Location = new System.Drawing.Point(72, 101);
+            this.button2_2.Name = "button2_2";
+            this.button2_2.Size = new System.Drawing.Size(40, 40);
+            this.button2_2.TabIndex = 4;
+            this.button2_2.UseVisualStyleBackColor = true;
+            this.button2_2.Click += new System.EventHandler(this.buttonClick);
+            // 
+            // button2_1
+            // 
+            this.button2_1.Font = new System.Drawing.Font("Consolas", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2_1.Location = new System.Drawing.Point(26, 101);
+            this.button2_1.Name = "button2_1";
+            this.button2_1.Size = new System.Drawing.Size(40, 40);
+            this.button2_1.TabIndex = 3;
+            this.button2_1.UseVisualStyleBackColor = true;
+            this.button2_1.Click += new System.EventHandler(this.buttonClick);
+            // 
+            // button3_3
+            // 
+            this.button3_3.Font = new System.Drawing.Font("Consolas", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3_3.Location = new System.Drawing.Point(118, 147);
+            this.button3_3.Name = "button3_3";
+            this.button3_3.Size = new System.Drawing.Size(40, 40);
+            this.button3_3.TabIndex = 8;
+            this.button3_3.UseVisualStyleBackColor = true;
+            this.button3_3.Click += new System.EventHandler(this.buttonClick);
+            // 
+            // button3_2
+            // 
+            this.button3_2.Font = new System.Drawing.Font("Consolas", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3_2.Location = new System.Drawing.Point(72, 147);
+            this.button3_2.Name = "button3_2";
+            this.button3_2.Size = new System.Drawing.Size(40, 40);
+            this.button3_2.TabIndex = 7;
+            this.button3_2.UseVisualStyleBackColor = true;
+            this.button3_2.Click += new System.EventHandler(this.buttonClick);
+            // 
+            // button3_1
+            // 
+            this.button3_1.Font = new System.Drawing.Font("Consolas", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3_1.Location = new System.Drawing.Point(26, 147);
+            this.button3_1.Name = "button3_1";
+            this.button3_1.Size = new System.Drawing.Size(40, 40);
+            this.button3_1.TabIndex = 6;
+            this.button3_1.UseVisualStyleBackColor = true;
+            this.button3_1.Click += new System.EventHandler(this.buttonClick);
+            // 
+            // button_newgame
+            // 
+            this.button_newgame.Font = new System.Drawing.Font("Consolas", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button_newgame.Location = new System.Drawing.Point(188, 55);
+            this.button_newgame.Name = "button_newgame";
+            this.button_newgame.Size = new System.Drawing.Size(75, 132);
+            this.button_newgame.TabIndex = 9;
+            this.button_newgame.Text = "New Game";
+            this.button_newgame.UseVisualStyleBackColor = true;
+            this.button_newgame.Click += new System.EventHandler(this.button_newgame_Click);
+            // 
+            // labelTurn
+            // 
+            this.labelTurn.AutoSize = true;
+            this.labelTurn.Font = new System.Drawing.Font("Consolas", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelTurn.Location = new System.Drawing.Point(50, 18);
+            this.labelTurn.Name = "labelTurn";
+            this.labelTurn.Size = new System.Drawing.Size(90, 22);
+            this.labelTurn.TabIndex = 10;
+            this.labelTurn.Text = "X\'s Turn";
+            this.labelTurn.Visible = false;
+            // 
+            // MainWin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 226);
+            this.Controls.Add(this.labelTurn);
+            this.Controls.Add(this.button_newgame);
+            this.Controls.Add(this.button3_3);
+            this.Controls.Add(this.button3_2);
+            this.Controls.Add(this.button3_1);
+            this.Controls.Add(this.button2_3);
+            this.Controls.Add(this.button2_2);
+            this.Controls.Add(this.button2_1);
+            this.Controls.Add(this.button1_3);
+            this.Controls.Add(this.button1_2);
+            this.Controls.Add(this.button1_1);
+            this.Name = "MainWin";
+            this.ShowIcon = false;
+            this.Text = "TicTacToe";
+            this.Load += new System.EventHandler(this.MainWin_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1_1;
+        private System.Windows.Forms.Button button1_2;
+        private System.Windows.Forms.Button button1_3;
+        private System.Windows.Forms.Button button2_3;
+        private System.Windows.Forms.Button button2_2;
+        private System.Windows.Forms.Button button2_1;
+        private System.Windows.Forms.Button button3_3;
+        private System.Windows.Forms.Button button3_2;
+        private System.Windows.Forms.Button button3_1;
+        private System.Windows.Forms.Button button_newgame;
+        private System.Windows.Forms.Label labelTurn;
+    }
+}
+
---
+++ CSharpTicTacToe/CSharpTicTacToe/Form1.cs
@@ -0,0 +1,225 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CSharpTicTacToe
+{
+    public partial class MainWin : Form
+    {
+        enum Player
+        {
+            X,O
+        };
+
+        Player currentPlayer;
+        int moveLeft;
+
+        public MainWin()
+        {
+            InitializeComponent();
+        }
+
+        private void MainWin_Load(object sender, EventArgs e)
+        {
+            StartNewGame();
+        }
+
+        private void StartNewGame()
+        {
+            button1_1.Text = "";
+            button1_2.Text = "";
+            button1_3.Text = "";
+            button2_1.Text = "";
+            button2_2.Text = "";
+            button2_3.Text = "";
+            button3_1.Text = "";
+            button3_2.Text = "";
+            button3_3.Text = "";
+
+            button1_1.Enabled = true;
+            button1_2.Enabled = true;
+            button1_3.Enabled = true;
+            button2_1.Enabled = true;
+            button2_2.Enabled = true;
+            button2_3.Enabled = true;
+            button3_1.Enabled = true;
+            button3_2.Enabled = true;
+            button3_3.Enabled = true;
+
+            labelTurn.Visible = true;
+
+            currentPlayer = Player.X;
+            moveLeft = 9;
+        }
+
+        private void EndGame()
+        {
+            button1_1.Enabled = false;
+            button1_2.Enabled = false;
+            button1_3.Enabled = false;
+            button2_1.Enabled = false;
+            button2_2.Enabled = false;
+            button2_3.Enabled = false;
+            button3_1.Enabled = false;
+            button3_2.Enabled = false;
+            button3_3.Enabled = false;
+        }
+
+        private void CheckRow()
+        {
+            if (button1_1.Text == button1_2.Text && button1_2.Text == button1_3.Text)
+            {
+                if (button1_1.Text == Player.X.ToString())
+                {
+                    EndGame();
+                    MessageBox.Show(Player.X.ToString() + " has won the game!");
+                }
+                else
+                if (button1_1.Text == Player.O.ToString())
+                {
+                    EndGame();
+                    MessageBox.Show(Player.O.ToString() + " has won the game!");
+                }
+            }
+            else
+            if (button2_1.Text == button2_2.Text && button2_2.Text == button2_3.Text)
+            {
+                if (button2_1.Text == Player.X.ToString())
+                {
+                    EndGame();
+                    MessageBox.Show(Player.X.ToString() + " has won the game!");
+                }
+                else
+                if (button2_1.Text == Player.O.ToString())
+                {
+                    EndGame();
+                    MessageBox.Show(Player.O.ToString() + " has won the game!");
+                }
+            }
+            else
+            if (button3_1.Text == button3_2.Text && button3_2.Text == button3_3.Text)
+            {
+                if (button3_1.Text == Player.X.ToString())
+                {
+                    EndGame();
+                    MessageBox.Show(Player.X.ToString() + " has won the game!");
+                }
+                else
+                if (button3_1.Text == Player.O.ToString())
+                {
+                    EndGame();
+                    MessageBox.Show(Player.O.ToString() + " has won the game!");
+                }
+            }
+        }
+
+        private void CheckColumn()
+        {
+            if (button1_1.Text == button2_1.Text && button2_1.Text == button3_1.Text)
+            {
+                if (button1_1.Text == Player.X.ToString())
+                {
+                    EndGame();
+                    MessageBox.Show(Player.X.ToString() + " has won the game!");
+                }
+                else
+                if (button1_1.Text == Player.O.ToString())
+                {
+                    EndGame();
+                    MessageBox.Show(Player.O.ToString() + " has won the game!");
+                }
+            }
+            else 
+            if (button1_2.Text == button2_2.Text && button2_2.Text == button3_2.Text)
+            {
+                if (button1_2.Text == Player.X.ToString())
+                {
+                    EndGame();
+                    MessageBox.Show(Player.X.ToString() + " has won the game!");
+                }
+                else
+                if (button1_2.Text == Player.O.ToString())
+                {
+                    EndGame();
+                    MessageBox.Show(Player.O.ToString() + " has won the game!");
+                }
+            }
+            else
+            if (button1_3.Text == button2_3.Text && button2_3.Text == button3_3.Text)
+            {
+                if (button1_3.Text == Player.X.ToString())
+                {
+                    EndGame();
+                    MessageBox.Show(Player.X.ToString() + " has won the game!");
+                }
+                else 
+                if (button1_3.Text == Player.O.ToString())
+                {
+                    EndGame();
+                    MessageBox.Show(Player.O.ToString() + " has won the game!");
+                }
+            }
+        }
+
+        private void CheckDiagonal()
+        {
+            if (button1_1.Text == button2_2.Text && button2_2.Text == button3_3.Text)
+            {
+                if (button1_1.Text == Player.X.ToString())
+                {
+                    EndGame();
+                    MessageBox.Show(Player.X.ToString() + " has won the game!");
+                }
+                else
+                if (button1_1.Text == Player.O.ToString())
+                {
+                    EndGame();
+                    MessageBox.Show(Player.O.ToString() + " has won the game!");
+                }
+            }
+            else
+            if (button1_3.Text == button2_2.Text && button2_2.Text == button3_1.Text)
+            {
+                if (button1_3.Text == Player.X.ToString())
+                {
+                    EndGame();
+                    MessageBox.Show(Player.X.ToString() + " has won the game!");
+                }
+                else
+                if (button1_3.Text == Player.O.ToString())
+                {
+                    EndGame();
+                    MessageBox.Show(Player.O.ToString() + " has won the game!");
+                }
+            }
+        }
+
+        private void Check()
+        {
+            CheckRow();
+            CheckColumn();
+            CheckDiagonal();
+        }
+
+        private void button_newgame_Click(object sender, EventArgs e)
+        {
+            StartNewGame();
+        }
+
+        private void buttonClick(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+            button.Text = currentPlayer.ToString();
+            currentPlayer = currentPlayer == Player.X ? Player.O : Player.X;
+            button.Enabled = false;
+            labelTurn.Text = currentPlayer.ToString() + "'s Turn";
+            Check();
+        }
+    }
+}
